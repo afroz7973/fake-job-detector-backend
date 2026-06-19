2eb42da13533e3efca6d8950c18317cd1f4cadbb
afroj-5555 <afrojentrepreneur@gmail.com>
2026-06-19 18:41:21 +0530

    fix deployment settings

--- core/settings.py
+++ core/settings.py
@@ -85,18 +85,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fake_job_detector',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'default': dj_database_url.config(
+    'default': dj_database_url.config(
         default=config('DATABASE_URL')
-        )
-    }
+    )
 }
 
 REST_FRAMEWORK = {
